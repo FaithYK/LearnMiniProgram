bc972424b2ef40bdb04f290b7237cdc5c3ef68bc
yk <faith_yangkui@163.com>
2019-11-25 18:43:30 +0800

    提交

--- app.js
+++ app.js
@@ -3,28 +3,46 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    
+  onLaunch: function() {
+    //获取用户信息
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
-  onShow: function (options) {
-    
+  onShow: function(options) {
+    //1.判断小程序的进入场景
+    console.log(options)
+    switch (options.scene) {
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
+
+    //2.獲取用戶信息，並且獲取到用戶信息之後，將用戶的信息傳遞給服務器
+   wx.getUserInfo({
+     success: function(res){
+      console.log(res)
+     }
+   })
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
-  onError: function (msg) {
-    
+  onError: function(msg) {
+
+  },
+  globalData:{
+    name: 'KK',
+    age: 19
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,50 @@
 // pages/home/home.js
+const appInstance = getApp()
+
+console.log(appInstance.globalData.name)
+console.log(appInstance.globalData.age)
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name: 'KK',
+    age: 18,
+    person: [
+      {id: 110, name: 'YK', age: 18},
+      { id: 111, name: 'YY', age: 18 },
+      { id: 112, name: 'KK', age: 18 },
+      { id: 113, name: 'GG', age: 18 }
+    ],
+    counter: 0,
+    nickName: '',
+    list:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //var self = this
+    wx.request({
+      url: 'http://localhost:3000/data',
+      // success: function(res){
+      //   console.log(res)
+      //   const data = res.data;
+      //   console.log(data)
+      //   self.setData({
+      //     list: data
+      //   })
+      // }
+      success: (res) =>{
+        const data = res.data;
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
 
   /**
@@ -67,5 +99,39 @@ Page({
     wx.navigateTo({
       url: '/pages/about/about',
     })
-  }
+  },
+  //点击增加
+  count: function(){
+    // 1. this.data.counter += 1   counter值变化了，但是界面上无变化
+    //console.log(this.data.counter)
+
+    // 2. this.setData(
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  reduce: function(){
+      this.setData({
+        counter: this.data.counter -1
+      })
+  },
+  //獲取用戶信息
+  getUserInfo: function(){
+    var infoThis = this
+    wx.getUserInfo({
+      success: function(res){
+        console.log(res.userInfo.nickName)
+        infoThis.setData({
+          nickName: res.userInfo.nickName
+        })
+      }
+    })
+  },
+ 
+ onPullDownRefresh:function(){
+   console.log('下拉刷新事件')
+ },
+
+
+
 })
